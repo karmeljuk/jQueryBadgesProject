f100b541105a599c55de64663ff73532ac85d336
karmeljuk <karmeljuk@gmail.com>
2017-11-19 04:53:05 +0200

    Finish changes

--- assets/main.js
+++ assets/main.js
@@ -1,5 +1,17 @@
 $(function() {
-
-  // your code will go here
-
+	$.ajax({
+		url: 'https://www.codeschool.com/users/1032742.json', 
+		dataType: 'jsonp', 
+		success: function (response) {
+			$.each(response.courses.completed, function () {
+				$('#badges').append(
+						'<div class="course">' + 
+							`<h3>${this.title}</h3>`+
+							`<img src="${this.badge}">`+
+							`<a class="btn btn-primary" href="${this.url}" target="_blank">See Course</a>`+
+						'</div>'
+					);
+			})
+		}
+	})
 });
